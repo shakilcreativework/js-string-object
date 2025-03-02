9597dfcc9f80ff6dbcf6963c58dadba906ff3d15
Shakil Ahmed <shakil.devcraft@gmail.com>
2025-03-02 15:40:17 +0600

    trim remove white space after, before or between with comperison

---
+++ case.js
@@ -0,0 +1,22 @@
+const school = 'Raj Uk Uttara Model School';
+
+console.log(school);
+
+
+// const subject = 'Chemistry';
+// const book = 'chemistry';
+
+const subject = 'Chemistry';
+const book = 'chemistry';
+
+if(subject === book){
+    console.log('I am reading book ai bar fatai felmu');
+}else{
+    console.log('I am reading book for biye next season');
+}
+
+if(subject.toLowerCase() === book.toLowerCase()){
+    console.log('I am reading book ai bar fatai felmu');
+}else{
+    console.log('I am reading book for biye next season');
+}
---
+++ js-string-object.js
@@ -0,0 +1,18 @@
+// const country = 'Bangladesh';
+// const division = "New York";
+// const district = `B-Baria`;
+
+// const thana = new String('Demra');
+// console.log(typeof thana);
+
+
+// const numbers = [54, 64, 64, 64, 74, 74, 74, 44];
+// console.log(numbers.length);
+// console.log(numbers[1]);
+
+
+const capital = 'Dha ka';
+// console.log(capital.length);
+// console.log(capital[0]);
+capital[1] = 'O';
+console.log(capital);
