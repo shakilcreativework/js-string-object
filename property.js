e65eb6988a8a125a16562f1196c3f27767cb63e8
Shakil Ahmed <shakil.devcraft@gmail.com>
2025-03-02 19:26:59 +0600

    object property access ways

---
+++ property.js
@@ -0,0 +1,25 @@
+const person = {
+    name: 'Shakil Ahmed',
+    age: 25,
+    profession: 'developer',
+    salary: 25000,
+    married: true
+};
+
+
+// console.log(person.name);
+// console.log(person.age);
+// console.log(person.profession);
+// console.log(person.salary);
+// console.log(person.married);
+
+
+const income = person.salary;
+// console.log(income);
+// console.log(person['name']);
+// console.log(person['age']);
+// console.log(person['profession']);
+// console.log(person['salary']);
+// console.log(person['married']);
+
+
