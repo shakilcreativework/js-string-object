45c404ba97e8e77fe3fdd5bc3f4691e143d74e8a
Shakil Ahmed <shakil.devcraft@gmail.com>
2025-03-02 22:34:57 +0600

    nested object in use object and array value

---
+++ nested.js
@@ -0,0 +1,20 @@
+const college = {
+    name: 'vnc',
+    class: ['11', '12'],
+    events: ['science fair', 'bijoy dibos', '21 Fed'],
+    speciality: {
+        color: 'blue',
+        result: {
+            gpa: 5,
+            merit: 'top'
+        }
+    }
+};
+
+// console.log(college);
+// console.log(college.speciality.color);
+// college.speciality.result.gpa = 4.5;
+// college.events[1] = 'Sadinota dibos';
+// console.log(college.events[1]);
+// delete college.speciality.color;
+console.log(college);
