307d51d22c65b8921d2ee735e2e44c0c93c2ddfb
Shakil Ahmed <shakil.devcraft@gmail.com>
2025-03-02 22:13:04 +0600

    again, learn more how can use properly object

--- object.js
+++ object.js
@@ -6,4 +6,43 @@ let isDeveloper;
 isDeveloper = 'Web developer';
 
 const subjects = ['bangla', 'english', 'physics', 'math'];
-console.log(subjects);
+// console.log(subjects);
+
+
+const bottle = {
+    brand: 'apple',
+    price: 45,
+    color: 'white',
+    isClean: false
+};
+
+
+// dot notation
+// console.log(bottle);
+// console.log(bottle.brand);
+// console.log(bottle.price);
+// console.log(bottle.color);
+// console.log(bottle.isClean);
+
+
+// backet notation
+// console.log(bottle['brand']);
+// console.log(bottle['price']);
+// console.log(bottle['color']);
+// console.log(bottle['isClean']);
+
+
+// property value write an other variable same like value declaration in or with ''
+const bottleColor = bottle.color;
+// console.log(bottleColor);
+
+
+const subject = {
+    name: 'biology',
+    teacher: 'rasheda mam',
+    examDate: '30 Feb',
+    chapters: ['first', 'second', 'third'],
+    exams: {
+        name: 'Final Exam',
+    }
+};
