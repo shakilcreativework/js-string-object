d542e49855b762ad825795bc2b630dee0ee910e7
Shakil Ahmed <shakil.devcraft@gmail.com>
2025-03-02 16:05:55 +0600

    how to next by next string by concat

---
+++ slice.js
@@ -0,0 +1,36 @@
+// const address = 'Mamudakola';
+// const part = address.slice(1, 4);
+
+// console.log(part);
+
+
+// const sentence = 'I am a good and hardworking person.';
+// console.log(sentence.split(' '));
+// console.log(sentence.split('a'));
+// console.log(sentence.toLowerCase().split('i'));
+
+
+// const friendsStr = 'Rahim,kahim,dahim,lahim,fahim,sahim';
+// const friends = friendsStr.split(',');
+// console.log(friends);
+
+// const realFriend = [ 'Rahim', 'kahim', 'dahim', 'lahim', 'fahim', 'sahim' ];
+// console.log(realFriend);
+
+// console.log(realFriend.join());
+// console.log(realFriend.join('|'));
+// console.log(realFriend.join('-'));
+
+
+const first = 'Abid';
+const last = 'Navid';
+
+const fullName = first + last;
+const fullNameWithSpace = first + ' ' + last;
+const fullName2 = first.concat(last);
+console.log(fullNameWithSpace);
+
+
+// includes
+console.log(last.includes('x'));
+console.log(last.includes('a'));
